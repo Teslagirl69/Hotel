768a9940c6fa6e58b15024bdbcffaf270727c250
Teslagirl69 <teslagirl17@gmail.com>
2022-09-28 14:17:30 +0300

    make just one devise_for

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,17 @@
 Rails.application.routes.draw do
+  resources :rooms
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+   root "rooms#index"
 
         devise_for :admins, path: 'admins', controllers: {
           registrations: 'admins/registrations'
         }
-        devise_for :admins, path: 'admins', controllers: {
-          sessions: 'admins/sessions'
-        }
-        devise_scope :admin do
+               devise_scope :admin do
           authenticated :admin do
             namespace :admins do
-              get 'admins//index', as: :authenticated_root
+              get 'admins/index', as: :authenticated_root
               get '/admins/sign_out' => 'devise/sessions#destroy'
             end
           end
