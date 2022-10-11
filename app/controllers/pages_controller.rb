4913df1f3f2e42932170be12e9eb46cd0c48e437
Teslagirl69 <teslagirl17@gmail.com>
2022-10-11 23:38:38 +0300

    merge api_weather

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   def index
+
     @rooms = Room.all.first(3)
+    data = CurrentWeatherService.new(latitude: "59.93", longitude: "30.33", units: "metric").call
+    @weather = Weather.new(data)
   end
 end
