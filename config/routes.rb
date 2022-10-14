60abcf7fc639200c2f2002dc0b95a0cda47cd0c9
Teslagirl69 <teslagirl17@gmail.com>
2022-10-14 17:51:16 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -41,36 +41,4 @@ Rails.application.routes.draw do
       root :to => 'session#new', as: :unauthenticated_root
     end
   end
-
-
-
-#   resources :pages
-#
-#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-#
-#   # Defines the root path route ("/")
-#    root "pages#index"
-#
-#         devise_for :admin, path: 'admin', controllers: {
-#           registrations: 'admin/registrations'
-#         }, :sign_out_via => [ :get ]
-#                devise_scope :admin do
-#           authenticated :admin do
-#             namespace :admin do
-#               get 'dashboard', to: 'pages#dashboard'
-#                get '/admin/dashboard', as: :authenticated_root
-#               # get '/admin/sign_out' => 'devise/sessions#destroy'
-#               resources :rooms
-#               # resources :pages
-#             end
-#           end
-#           unauthenticated :admin do
-#             namespace :admin do
-#               root :to => 'session#new', as: :unauthenticated_root
-#           end
-#          end
-#   # authenticated :admin do
-#   #   root to: 'devise/sessions#new', as: :admin_authenticated_root_path
-#   # end
-# end
 end
