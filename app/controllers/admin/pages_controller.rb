56a7a195d840552d8b5edf432db401efaecb4318
Teslagirl69 <teslagirl17@gmail.com>
2022-10-17 03:32:56 +0300

    rubocop on admin controllers

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
-
-  class Admin::PagesController < ApplicationController
-    before_action :authenticate_admin!
-    def dashboard
-      @bookings = Booking.all.order('bookings.created_at desc')
-      @caclulations = CalculateDailyStatService.new.calculate
-    end
+class Admin::PagesController < ApplicationController
+  before_action :authenticate_admin!
+  def dashboard
+    @bookings = Booking.all.order('bookings.created_at desc')
+    @caclulations = CalculateDailyStatService.new.calculate
   end
-
+end
--- app/controllers/admin/registrations_controller.rb
+++ app/controllers/admin/registrations_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-
-  class Admin::RegistrationsController < Devise::RegistrationsController
-    include Accessible
-    skip_before_action :check_resource, except: [new, :create]
-  end
-
+class Admin::RegistrationsController < Devise::RegistrationsController
+  include Accessible
+  skip_before_action :check_resource, except: [new, :create]
+end
