d25289f2d1a04a73f90c380a2011b0c48bb6c831
Teslagirl69 <teslagirl17@gmail.com>
2022-10-17 14:30:15 +0300

    update csv job

--- app/services/export_bookings_to_csv.rb
+++ app/services/export_bookings_to_csv.rb
@@ -6,8 +6,10 @@ class ExportBookingsToCsv
   end
 
   def call
-    CSV.open('./files/bookings.csv', 'a+') do |csv|
       @last_booking = Booking.all.accepted.order(updated_at: :desc).first
+      return unless @last_booking
+
+      CSV.open('./files/bookings.csv', 'a+') do |csv|
       csv << [@last_booking.id, @last_booking.start_date, @last_booking.last_date, @last_booking.created_at,
               @last_booking.user_name, @last_booking.user_email, @last_booking.room_id]
     end
